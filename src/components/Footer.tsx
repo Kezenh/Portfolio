4de0b179841b5650b239072110a3963ab6b2a118
Kezenh <quentin.rampteau@gmail.com>
2026-09-22 01:08:10 +0200

    Sticky header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="border-t-4 border-slate-700 p-4">
+    <footer className="border-t-4 border-slate-700 p-4 mt-4">
       <nav className="flex justify-start gap-6">
         <a href="https://github.com/Kezenh" target="_blank" rel="noopener noreferrer" className="transition-colors hover:text-slate-400">GitHub</a>
-        <a href="#" target="_blank" rel="noopener noreferrer" className="transition-colors hover:text-slate-400">LinkedIn &lt;-- A FAIRE</a>
+        <a href="https://www.linkedin.com/in/quentin-rampteau" target="_blank" rel="noopener noreferrer" className="transition-colors hover:text-slate-400">LinkedIn</a>
         <a href="mailto:quentin.rampteau@gmail.com" className="transition-colors hover:text-slate-400">quentin.rampteau@gmail.com</a>
       </nav>
     </footer>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="border-b-4 border-slate-700 p-4">
+    <header className="border-b-4 border-slate-700 p-4 sticky top-0 bg-slate-950">
       <h1 className="text-3xl font-bold">Quentin Rampteau</h1>
     </header>
   );
