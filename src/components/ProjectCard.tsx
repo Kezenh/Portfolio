0776dc584faccd695f58dae95c1854e3b99a8d5a
Kezenh <quentin.rampteau@gmail.com>
2026-09-22 17:24:18 +0200

    Improving responsivness

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -19,9 +19,9 @@ export default function ProjectCard({ name, url, description }: ProjectCardProps
     images[`../assets/images/cards images/${name}.png`];
 
   return (
-    <a className="bg-slate-400 dark:bg-slate-800 p-4 rounded-lg w-96 h-96 flex flex-col" href={url} target="_blank" rel="noopener noreferrer">
-      <img className="min-h-32 object-contain" src={imageUrl} alt={name} />
-      <div dangerouslySetInnerHTML={{ __html: description }} className="whitespace-pre-line text-justify h-full flex flex-col justify-between" />
+    <a className="bg-slate-400 dark:bg-slate-800 p-4 rounded-lg w-[80vw] max-w-96 aspect-square flex flex-col" href={url} target="_blank" rel="noopener noreferrer">
+      <img className="h-1/4 object-contain" src={imageUrl} alt={name} />
+      <div dangerouslySetInnerHTML={{ __html: description }} className="h-3/4 grid gap-4 overflow-y-auto scrollbar-thin scrollbar-thumb-slate-500 scrollbar-track-slate-300 dark:scrollbar-thumb-slate-500 dark:scrollbar-track-slate-700 p-4 pr-0 scrollbar-gutter-stable" />
     </a>
   );
 }
