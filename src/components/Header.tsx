3a73c9416f3a22a5ede2ee08d6ed76ef331146e3
Kezenh <quentin.rampteau@gmail.com>
2026-09-23 17:15:18 +0200

    Adding gap on responsive header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   const { language, toggleLanguage } = useLanguage();
 
   return (
-    <header className="border-b-4 border-slate-700 p-4 sticky top-0 z-50 bg-slate-300 dark:bg-slate-950 flex w-full justify-between items-center flex-wrap">
+    <header className="border-b-4 border-slate-700 p-4 sticky top-0 z-50 bg-slate-300 dark:bg-slate-950 flex w-full justify-between items-center flex-wrap gap-4">
       <h1 className="text-3xl font-bold">Quentin Rampteau</h1>
       <div className="flex gap-4">
         <button onClick={toggleLanguage} className="w-15 flex items-center justify-center aspect-square px-4 py-2 border-2 border-slate-500 font-pixel text-xl hover:bg-slate-200 dark:hover:bg-slate-800 active:translate-y-1 transition-transform cursor-pointer" aria-label={theme === 'dark' ? 'Passer en mode clair' : 'Passer en mode sombre'} title={theme === 'dark' ? 'Mode clair' : 'Mode sombre'}>
