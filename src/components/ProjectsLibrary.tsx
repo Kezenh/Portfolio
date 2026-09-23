5e2a7754c7e5aba8b030abb9321b04b9e6f04060
Kezenh <quentin.rampteau@gmail.com>
2026-09-23 17:05:22 +0200

    Fixing translation

--- src/components/ProjectsLibrary.tsx
+++ src/components/ProjectsLibrary.tsx
@@ -12,7 +12,7 @@ export default function ProjectsLibrary() {
   return (
         <>
             <button className="m-4 px-4 py-2 border-2 border-slate-500 font-pixel text-xl hover:bg-slate-200 dark:hover:bg-slate-800 active:translate-y-1 transition-transform cursor-pointer" onClick={() => setReversed(!reversed)}>
-                Les plus récents {reversed ? "\u2191" : "\u2193"}
+                {language === 'fr' ? 'Les plus récents' : 'Most recents'} {reversed ? "\u2191" : "\u2193"}
             </button>
             <div className="grid grid-cols-1 sm:grid-cols-[repeat(auto-fit,minmax(24rem,1fr))] justify-items-center mx-auto w-full gap-4 px-4">
                 {sortedProjects.map((project) => (
